2cae4533fd8b76a20201a97df19b18c435eb8aa8
Nick Dumas <drakonik@gmail.com>
2014-12-30 04:51:54 -0500

    Initial BetterOrderedDict implementation.

---
+++ better_od.py
@@ -0,0 +1,35 @@
+from collections.abc import MutableMapping
+
+
+class NewOrderedDict(MutableMapping):
+    def __init__(self, **kwargs):
+        self._d = dict()
+        self._keys = []
+
+    def __len__(self):
+        return len(self._d)
+
+    def __iter__(self):
+        for key in self._keys:
+            yield self._d[key]
+
+    def __setitem__(self, key, value):
+        self._keys.append(key)
+        self._d[key] = value
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    def __delitem__(self, key):
+        del self._d[key]
+
+    def insert(self, key, value, index):
+        if key in self._d:
+            self._keys.pop(key)
+        self._keys.insert(index, key)
+        self._d[key] = value
+
+    def reorder_keys(self, keys):
+        if self._keys != self._d:
+            raise ValueError('Keys do not match.')
+        self._keys = keys
