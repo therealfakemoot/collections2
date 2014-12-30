8f0b72dcad39fbd6072185bf2244eb75f0f45a96
Nick Dumas <drakonik@gmail.com>
2014-12-30 05:36:40 -0500

    Add docstrings for most public API methods.

--- better_od/core.py
+++ better_od/core.py
@@ -2,7 +2,14 @@ from collections import MutableMapping
 
 
 class BetterOrderedDict(MutableMapping):
+    '''BetterOrderedDict is a mapping object that allows for ordered access
+    and insertion of keys. With the exception of the key_index, insert, and
+    reorder_keys methods behavior is identical to stock dictionary objects.'''
+
     def __init__(self, **kwargs):
+        '''BetterOrderedDict accepts an optional iterable of two-tuples
+        indicating keys and values.'''
+
         self._d = dict()
         self._keys = []
 
@@ -25,15 +32,22 @@ class BetterOrderedDict(MutableMapping):
         del self._d[key]
 
     def key_index(self, key):
+        '''Accepts a parameter, :key:, and returns an integer value
+        representing its index in the ordered list of keys.'''
         return self._keys.index(key)
 
     def insert(self, key, value, index):
+        '''Accepts a :key:, :value:, and :index: parameter and inserts
+        a new key, value member at the desired index.'''
         if key in self._d:
             self._keys.pop(key)
         self._keys.insert(index, key)
         self._d[key] = value
 
     def reorder_keys(self, keys):
+        '''Accepts a :keys: parameter, an iterable of keys in the
+        desired new order. The :keys: parameter must contain all
+        existing keys.'''
         if self._keys != self._d:
             raise ValueError('Keys do not match.')
         self._keys = keys
