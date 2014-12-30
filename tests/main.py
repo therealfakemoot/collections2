48c186d2fc6b388a5f7a2a085ea166f931cdd80a
Justus Wingert <juw@solute.de>
2014-12-30 11:46:07 +0100

    First test scenario added.

--- tests/main.py
+++ tests/main.py
@@ -7,3 +7,10 @@ def test_insert():
     d.insert('foo', 'bar', 2)
     assert d['foo'] == 'bar'
     assert d.keys()[0] == 'foo'
+
+    d.insert('a', 'b', 2)
+    d.insert('c', 'd', 2)
+    d.insert('e', 'f', 2)
+
+    assert d['foo'] == 'bar'
+    assert d.keys()[0] == 'foo'
