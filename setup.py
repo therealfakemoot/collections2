f22de37a1e88c8e0bac8a72a0f3a605ebdceacb2
Nick Dumas <drakonik@gmail.com>
2014-12-30 05:06:44 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='better_od',
+    description='A more flexible implementation of OrderedDict',
+    author='Nick Dumas',
+    author_email='drakonik@gmail.com',
+    url='https://github.com/therealfakemoot/BetterOrderedDict/',
+    packages=find_packages(),
+    zip_safe=False
+)
