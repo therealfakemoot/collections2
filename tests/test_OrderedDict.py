0f4324be199d683623618031e36954811fac6d6c
Nick Dumas <drakonik@gmail.com>
2015-01-01 14:03:35 -0500

    Renaming to make tests pass after renaming OrderedDict.

--- tests/test_OrderedDict.py
+++ tests/test_OrderedDict.py
@@ -1,43 +1,43 @@
 from twisted.trial import unittest
 from twisted.trial.itrial import IReporter
 
-from better_od import BOD
+from better_od import OrderedDict
 
 
 class DocstringReporter(IReporter):
     pass
 
 
-class TestBOD(unittest.TestCase):
+class TestOrderedDict(unittest.TestCase):
     def setUp(self):
         self._test_keys = ['a', 'b', 'c']
         self._test_vals = [-2, 0, 1]
-        self.d = BOD(zip(self._test_keys, self._test_vals))
+        self.d = OrderedDict(zip(self._test_keys, self._test_vals))
 
-    def test_BOD_order(self):
+    def test_OrderedDict_order(self):
         self.assertEquals(self.d.keys(), [key for key in self._test_keys])
 
-    def test_BOD_index(self):
+    def test_OrderedDict_index(self):
         self.assertEquals(self.d.key_index('c'), 2)
 
-    def test_BOD_reorder(self):
+    def test_OrderedDict_reorder(self):
         new_order = ['c', 'a', 'b']
         self.d.reorder_keys(new_order)
         self.assertEquals(self.d.keys(), new_order)
 
 
-class TestBODEquality(unittest.TestCase):
+class TestOrderedDictEquality(unittest.TestCase):
     def setUp(self):
         self._test_keys = ['a', 'b', 'c']
         self._test_vals = [-2, 0, 1]
-        self.d = BOD(zip(self._test_keys, self._test_vals))
+        self.d = OrderedDict(zip(self._test_keys, self._test_vals))
 
     def test_order_check(self):
-        bod = BOD([('c', 1), ('b', 0), ('a', -2)])
+        bod = OrderedDict([('c', 1), ('b', 0), ('a', -2)])
         self.assertNotEquals(self.d, bod)
 
     def test_value_check(self):
-        bod = BOD([('a', -2), ('b', 0), ('c', 3)])
+        bod = OrderedDict([('a', -2), ('b', 0), ('c', 3)])
         self.assertNotEquals(self.d, bod)
 
     def test_builtin_dict_inequality(self):
