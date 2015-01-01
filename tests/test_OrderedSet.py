f3c11599ef1714f7337191719172614c43b87eff
Nick Dumas <drakonik@gmail.com>
2015-01-01 14:35:40 -0500

    Add tests.test_OrderedSet.

---
+++ tests/test_OrderedSet.py
@@ -0,0 +1,21 @@
+from twisted.trial import unittest
+
+from better_od import OrderedSet
+
+
+class TestOrderedDict(unittest.TestCase):
+    def setUp(self):
+        self.values = 'abcddefg'
+        self.s = OrderedSet(self.values)
+
+    def test_order(self):
+        expected = list(enumerate('abcdefg'))
+        self.assertEquals(list(enumerate(self.s)), expected)
+
+    def test_index(self):
+        self.assertEquals(self.s.key_index('c'), 2)
+
+    def test_add_new_value(self):
+        prev = len(self.s)
+        self.s.add('z')
+        self.assertEqual(len(self.s), prev + 1)
